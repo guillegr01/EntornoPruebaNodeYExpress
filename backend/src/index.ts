7a5e4d5e90787693872d8b5d4c0662658b74ce26
guillegr01 <gllrmglrncn2001@gmail.com>
2026-02-02 20:53:27 +0100

    segunda subida

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hola Mundo, desde  i backend");
+import express from 'express'
+import diaryRoutes from "./routes/diaries"
+
+//Creamos la API como tal
+const app = express();
+app.use(express.json());
+
+//Nos creamos un puerto concreto
+const PORT = 3000;
+
+//Endpoint
+app.get('/ping', (_req, res) => {
+    console.log('someone pinged here!!');
+    res.send('pong');
+});
+
+/** 
+ * * Asi es como asignamos a una ruta principal el router, dentro de esa ruta habrá
+ * * subrutas (las definidas en el router) que ejecutarán la correspondiente función del endpoint 
+ */
+app.use('/api/diaries', diaryRoutes);
+
+//Ponemos la PAI a esccuahr por un puerto
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);
+})
---
+++ backend/src/routes/diaries.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+
+/**
+ * * Los router son como unos manejadores o handlers donde vamos 
+ * * a designar los endpoints a las rutas especificas. 
+ * ?Por ejemplo: si tengo un tipo llamado users, me crearé un router para
+ * ?todos los endpoints de user, la función como tal del endpoint las haré
+ * ? en un archivo diferente en un carpeta resolvers.
+ * * Luego este router se importa en el archivo ejecutable y de la misma forma se le asigna una ruta principal.
+ */
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Fetching diary routes (GET)');
+});
+
+router.post('/', (req, res) => {
+    res.send('Fetching diary routes');
+});
+
+export default router;
